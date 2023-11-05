fd27ea8b5a18ebf994c353b6ee0de1eaa6610b94
Yaroslav <bakalyna.yaroslav@gmail.com>
2023-11-06 01:26:44 +0200

    finished project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,69 +1,18 @@
-import { useEffect, useState } from 'react';
-import { nanoid } from 'nanoid';
-import ContactForm from './ContactForm';
-import Filter from './Filter';
-import ContactList from './ContactList';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import styles from './App.module.css';
 
 
 const App = () => {
-  const [contacts, setContacts] = useState(getContactsInStorage());
-  const [filter, setFilter] = useState('');
-
-
-  function getContactsInStorage(){
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) return JSON.parse(savedContacts)
-    return [];
-  }
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-
-  const handleAddContact = (newContact) => {
-
-    const isContactExists = contacts.some((contact) => contact.name === newContact.name);
-
-    if (isContactExists) {
-      alert(`${newContact.name} вже є у вашій книзі!`);
-      return;
-    }
-
-    const contactToAdd = {
-      ...newContact,
-      id: nanoid(),
-    };
-
-    setContacts((prevState) =>
-      [...prevState, contactToAdd],
-    );
-  };
-
-  const addFilterContact = (e) => {
-    setFilter(e.target.value);
-  };
-
-  const getFilteredContacts = () => {
-    return contacts.filter(el =>
-      el.name.toLowerCase().includes(filter.toLowerCase()),
-    );
-  };
-
-  const onDeleteContact = id => {
-    setContacts(contacts.filter(item => item.id !== id))
-  };
-
   return (
-    <>
+    <div className={styles.container}>
       <h1>Phonebook</h1>
-      <ContactForm addNewContact={handleAddContact} />
+      <ContactForm />
       <h2>Contacts</h2>
-      <Filter contact={filter}
-              addFilterContact={addFilterContact} />
-      <ContactList onDeleteContact={onDeleteContact}
-                   getFilteredContacts={getFilteredContacts()} />
-    </>
+      <Filter />
+      <ContactList />
+    </div>
   );
 };
 
